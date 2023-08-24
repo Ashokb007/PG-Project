da61308402a23469bea065fbf0b2f0ba3c0677bc
Ashokb007 <142371052+Ashokb007@users.noreply.github.com>
2023-08-24 05:33:00 +0530

    Create parameter.tf

---
+++ TF scripts/parameter.tf
@@ -0,0 +1,11 @@
+variable "instance_name" {
+  description = "Instance name tagging"
+  type        = string
+  default     = "server"
+}
+
+variable "instance_team" {
+  description = "Instance Team tagging"
+  type        = string
+  default     = "R&D"
+}
