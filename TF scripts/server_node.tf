2b6d6c21abc2430a14376b1c4bac287681a72bf3
Ashokb007 <142371052+Ashokb007@users.noreply.github.com>
2023-08-24 05:37:14 +0530

    Create server_node.tf

---
+++ TF scripts/server_node.tf
@@ -0,0 +1,20 @@
+output "server_instance_public-ipv4" {
+  value = "65.2.167.199"          #public ip
+}
+output "server_instance_private_ipv4" {
+  value = "172.31.35.164"         #private ip
+}
+output "server_public_dns" {
+  value = "ip-172-31-35-164.ap-south-1.compute.internal"
+}
+
+
+output "node_instance_public-ipv4" {
+  value = "3.110.77.86"       #public ip
+}
+output "node_instance_private_ipv4" {
+  value = "172.31.34.159"     #private ip
+}
+output "node_public_dns" {
+  value = "ip-172-31-34-159.ap-south-1.compute.internal"
+}
